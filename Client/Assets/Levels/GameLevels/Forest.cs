a6371e18f2f7692f07b8653acaa8acd117f0a636
kornelijus.zubrickas <47833471+zebras159@users.noreply.github.com>
2020-10-04 14:20:31 +0300

    small fix

--- Client/Assets/Levels/GameLevels/Forest.cs
+++ Client/Assets/Levels/GameLevels/Forest.cs
@@ -18,7 +18,27 @@ namespace Client.Assets.Levels.GameLevels
         }
         public override void Render(PaintEventArgs e)
         {
-            base.Render(e);
+
+            e.Graphics.FillRectangle(Brushes.LightGreen, 0, 0, levelWidth, levelHeight);
+
+            int i = 0;
+            int j = 0;
+
+            while (i < horizontalBorders.Length / 2 || j < verticalBorders.Length / 2)
+            {
+                if (i < horizontalBorders.Length / 2)
+                {
+                    horizontalBorders[i, 0].Render(e);
+                    horizontalBorders[i, 1].Render(e);
+                    i++;
+                }
+                if (j < verticalBorders.Length / 2)
+                {
+                    verticalBorders[j, 0].Render(e);
+                    verticalBorders[j, 1].Render(e);
+                    j++;
+                }
+            }
         }
 
     }
