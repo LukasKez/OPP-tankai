e074120f244ad4de83a2a4aff8a1316cb7c88682
MantasAD <mantasadomav@gmail.com>
2020-10-18 23:47:39 +0300

    Implement observer pattern

---
+++ Client/Assets/Observer/IObserver.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Client
+{
+    interface IObserver
+    {
+        void OnSubjectUpdate();
+    }
+}
---
+++ Client/Assets/Observer/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Subject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void Attach(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void Detach(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        protected void Notify()
+        {
+            foreach (var observer in observers)
+            {
+                observer.OnSubjectUpdate();
+            }
+        }
+    }
+}
